89a73013718f5e41554cb0970fbfb23610f131cc
manuelmainz <manuel.mainz@googlemail.com>
2024-02-29 21:14:56 +0100

    setup project and added loading of rss fead

---
+++ RssFeedTranslator/RssFeedTranslator/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RssFeedTranslator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Xml;
+
+namespace RssFeedTranslator
+{
+    class MainWindowViewModel //: ObservableObject
+    {
+        private ArticleViewModel? selectedArticle;
+
+        public ICommand DoSomethingCommand { get; }
+
+        public ObservableCollection<ArticleViewModel> Articles { get; } = new ObservableCollection<ArticleViewModel>();
+
+        public ArticleViewModel? SelectedArticle
+        {
+            get => selectedArticle;
+            set => selectedArticle = value; // SetProperty(ref selectedArticle, value);
+        }
+
+        public MainWindowViewModel()
+        {
+             DoSomethingCommand = new RelayCommand(DoSomething);
+        }
+
+        public void DoSomething()
+        {
+            string url = "https://www.ad.nl/binnenland/rss.xml";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                Articles.Add(new ArticleViewModel(item));
+            }
+        }
+    }
+}
---
+++ RssFeedTranslator/RssFeedTranslator/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RssFeedTranslator.ViewModels
+{
+    class ArticleViewModel
+    {
+        private readonly SyndicationItem item;
+
+        public string Title => item.Title.Text;
+
+        public string Summary => item.Summary.Text;
+
+        public ArticleViewModel(SyndicationItem item)
+        {
+            this.item = item;
+        }
+    }
+}
